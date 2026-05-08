7974a87111ce956e1bd9a60c6ab4f8d9485960d0
abiabi31 <abishsabi31@gmail.com>
2026-05-08 22:20:58 +0530

    newchasw

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import ScrollToTop from "./utils/ScrollToTop/ScrollToTop";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
+
       <App />
     </BrowserRouter>
   </StrictMode>,
