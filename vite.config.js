a1252993cc5a97cb33bc4d24edc6c8be0a5bfa09
abiabi31 <abishsabi31@gmail.com>
2026-08-13 10:15:45 +0530

    half2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
+
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+
   server: {
     port: 3000,
+    strictPort: true,
   },
-
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 1000,
